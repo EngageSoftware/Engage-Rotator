a4e4bbca2ef1f13052dbc188b34a7f3e5a997d4c
Brian Dukes <bdukes@engagesoftware.com>
2009-02-24 14:42:09 +0000

    Documented Utility
    git-tfs-work-item:1375 associate
    git-tfs-id: [http://tfs.etg-inc.net:8080/tfs/Engage%20TFS%202010]$/Engage Rotator;C2441

--- Source/Utility.cs
+++ Source/Utility.cs
@@ -1,33 +1,60 @@
-//Copyright (c) 2004-2008
-//by Engage Software ( http://www.engagesoftware.net )
-
-//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
-//TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
-//THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
-//CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
-//DEALINGS IN THE SOFTWARE.
-
-using System.Globalization;
-using System.Text;
+// <copyright file="Utility.cs" company="Engage Software">
+// Engage: Rotator - http://www.engagemodules.com
+// Copyright (c) 2004-2009
+// by Engage Software ( http://www.engagesoftware.com )
+// </copyright>
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+// CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+// DEALINGS IN THE SOFTWARE.
 
 namespace Engage.Dnn.ContentRotator
 {
+    using System.Globalization;
+    using System.Text;
+
+    /// <summary>
+    /// How content is displayed
+    /// </summary>
     public enum DisplayType
-	{
+    {
+        /// <summary>
+        /// Content is not displayed
+        /// </summary>
         None = 0,
+
+        /// <summary>
+        /// Content is displayed by itself
+        /// </summary>
         Content,
+
+        /// <summary>
+        /// Content is displayed wrapped in a link
+        /// </summary>
         Link,
+
+        /// <summary>
+        /// Content is displayed wrapped in an event handler that causes the rotator tranistion to another piece of content
+        /// </summary>
         RotateContent
     }
 
-    public enum ControlPlacement
-    {
-        Above = 0,
-        Below
-    }
-
+    /// <summary>
+    /// Utility methods and common, static data for Engage: Rotator
+    /// </summary>
     internal static class Utility
     {
+        /// <summary>
+        /// The virtual path to the desktop module folder
+        /// </summary>
+        public const string DesktopModuleVirtualPath = "~/DesktopModules/EngageRotator/";
+
+        /// <summary>
+        /// The template folder name
+        /// </summary>
+        public const string StyleTemplatesFolderName = "StyleTemplates/";
+
         /// <summary>
         /// Adds a style declaration to the specified <see cref="StringBuilder"/> <paramref name="style"/>.
         /// Adds a style with the given <paramref name="styleName"/> and <paramref name="styleValue"/>.
@@ -43,8 +70,5 @@ namespace Engage.Dnn.ContentRotator
                 style.AppendFormat(CultureInfo.InvariantCulture, "{0}:{1}px;", styleName, styleValue.Value);
             }
         }
-
-        public const string DesktopModuleVirtualPath = "~/DesktopModules/EngageRotator/";
-        public const string StyleTemplatesFolderName = "StyleTemplates/";
     }
 }
