0df063ba7976f5373f46050d903d8d54952d889e
Brian Dukes <bdukes@engagesoftware.com>
2009-02-24 15:01:26 +0000

    Cleaned up TemplateManifest
    git-tfs-work-item:1375 associate
    git-tfs-id: [http://tfs.etg-inc.net:8080/tfs/Engage%20TFS%202010]$/Engage Rotator;C2443

--- Source/TemplateManifest.cs
+++ Source/TemplateManifest.cs
@@ -1,126 +1,126 @@
-//Copyright (c) 2004-2008
-//by Engage Software ( http://www.engagesoftware.net )
-
-//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
-//TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
-//THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
-//CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
-//DEALINGS IN THE SOFTWARE.
-
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Web.Hosting;
-using System.Xml;
-using System.Xml.Schema;
+// <copyright file="TemplateManifest.cs" company="Engage Software">
+// Engage: Rotator - http://www.engagemodules.com
+// Copyright (c) 2004-2009
+// by Engage Software ( http://www.engagesoftware.com )
+// </copyright>
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+// CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+// DEALINGS IN THE SOFTWARE.
 
 namespace Engage.Dnn.ContentRotator
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Web.Hosting;
+    using System.Xml;
+    using System.Xml.Schema;
+
+    /// <summary>
+    /// Represents a template manifest
+    /// </summary>
     internal class TemplateManifest
     {
-        #region Properties
-
-        private readonly string TemplateFilePath;
-
-        //[DebuggerBrowsable(DebuggerBrowsableState.Never)]
-        private string _templateName;
-        //public string TemplateName
-        //{
-        //    [DebuggerStepThrough]
-        //    get { return _templateName; }
-        //}
-
+        /// <summary>
+        /// Backing field for <see cref="Description"/>
+        /// </summary>
         [DebuggerBrowsable(DebuggerBrowsableState.Never)]
-        private readonly string _description;
-        public string Description
-        {
-            [DebuggerStepThrough]
-            get { return _description; }
-        }
+        private readonly string description;
 
-        //[DebuggerBrowsable(DebuggerBrowsableState.Never)]
-        private readonly string _stylesheetFilename;
-        //public string StylesheetFilename
-        //{
-        //    [DebuggerStepThrough]
-        //    get { return _stylesheetFilename; }
-        //}
-
-        public string StylesheetFilePath
-        {
-            get { return Engage.Utility.HasValue(_stylesheetFilename) ? TemplateFilePath + _stylesheetFilename : string.Empty; }
-        }
-
-        [DebuggerBrowsable(DebuggerBrowsableState.Never)] 
-        private readonly string _previewImageFilename;
-        public string PreviewImageFilename
-        {
-            [DebuggerStepThrough]
-            get { return _previewImageFilename; }
-        }
-
-        //public string PreviewImageFilePath
-        //{
-        //    get { return Engage.Utility.HasValue(_previewImageFilename) ? TemplateFilePath + _previewImageFilename : string.Empty; }
-        //}
-
+        /// <summary>
+        /// Backing field for <see cref="PreviewImageFilename"/>
+        /// </summary>
         [DebuggerBrowsable(DebuggerBrowsableState.Never)]
-        private Dictionary<string, string> _settings;
-        public IDictionary<string, string> Settings
-        {
-            [DebuggerStepThrough]
-            get { return new Dictionary<string, string>(_settings); }
-        }
+        private readonly string previewImageFilename;
 
-        #endregion
+        /// <summary>
+        /// Backing field for <see cref="StylesheetFilePath"/>
+        /// </summary>
+        private readonly string stylesheetFilename;
 
+        /// <summary>
+        /// The name of the template
+        /// </summary>
+        private readonly string templateName;
+
+        /// <summary>
+        /// Backing field for <see cref="StylesheetFilePath"/>
+        /// </summary>
+        private readonly string templateFilePath;
+
+        /// <summary>
+        /// Backing field for <see cref="Settings"/>
+        /// </summary>
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private Dictionary<string, string> settings;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TemplateManifest"/> class.
+        /// </summary>
+        /// <param name="filePath">The file path.</param>
+        /// <param name="templateName">Name of the template.</param>
         private TemplateManifest(string filePath, string templateName)
         {
-            _settings = new Dictionary<string, string>();
-            _templateName = templateName;
-            TemplateFilePath = Utility.DesktopModuleVirtualPath + Utility.StyleTemplatesFolderName + _templateName + "/";
+            this.settings = new Dictionary<string, string>();
+            this.templateName = templateName;
+            this.templateFilePath = Utility.DesktopModuleVirtualPath + Utility.StyleTemplatesFolderName
+                                    + this.templateName + "/";
             try
             {
-                using (FileStream manifestStream = new FileStream(HostingEnvironment.MapPath(filePath), FileMode.Open, FileAccess.Read))
+                using (
+                        FileStream manifestStream = new FileStream(
+                                HostingEnvironment.MapPath(filePath), FileMode.Open, FileAccess.Read))
                 {
                     XmlReaderSettings readerSettings = new XmlReaderSettings();
                     readerSettings.IgnoreWhitespace = true;
                     readerSettings.ValidationType = ValidationType.Schema;
-                    string schemaUri = (new Uri(HostingEnvironment.MapPath(Utility.DesktopModuleVirtualPath + Utility.StyleTemplatesFolderName + "Manifest.xsd"))).AbsoluteUri;
+                    string schemaUri =
+                            (new Uri(
+                                    HostingEnvironment.MapPath(
+                                            Utility.DesktopModuleVirtualPath + Utility.StyleTemplatesFolderName
+                                            + "Manifest.xsd"))).AbsoluteUri;
                     readerSettings.Schemas.Add(string.Empty, schemaUri);
                     using (XmlReader manifestReader = XmlReader.Create(manifestStream, readerSettings))
                     {
-                        //manifestReader.WhitespaceHandling = WhitespaceHandling.None;
+                        // manifestReader.WhitespaceHandling = WhitespaceHandling.None;
                         manifestReader.Read();
                         manifestReader.ReadStartElement("RotatorManifest");
                         if (manifestReader.IsStartElement("Description"))
                         {
                             manifestReader.ReadStartElement("Description");
-                            _description = manifestReader.ReadContentAsString();
-                            if (manifestReader.LocalName == "Description") //needed in case element is self-closing
+                            this.description = manifestReader.ReadContentAsString();
+                            if (manifestReader.LocalName == "Description")
                             {
-                                manifestReader.ReadEndElement(); //</description>
+                                // needed in case element is self-closing
+                                manifestReader.ReadEndElement(); // </description>
                             }
                         }
+
                         if (manifestReader.IsStartElement("Preview"))
                         {
                             manifestReader.ReadStartElement("Preview");
-                            _previewImageFilename = manifestReader.ReadContentAsString();
-                            if (manifestReader.LocalName == "Preview") //needed in case element is self-closing
+                            this.previewImageFilename = manifestReader.ReadContentAsString();
+                            if (manifestReader.LocalName == "Preview")
                             {
-                                manifestReader.ReadEndElement(); //</preview>
+                                // needed in case element is self-closing
+                                manifestReader.ReadEndElement(); // </preview>
                             }
                         }
+
                         if (manifestReader.IsStartElement("Stylesheet"))
                         {
                             manifestReader.ReadStartElement("Stylesheet");
-                            _stylesheetFilename = manifestReader.ReadContentAsString();
-                            if (manifestReader.LocalName == "Stylesheet") //needed in case element is self-closing
+                            this.stylesheetFilename = manifestReader.ReadContentAsString();
+                            if (manifestReader.LocalName == "Stylesheet")
                             {
-                                manifestReader.ReadEndElement(); //</stylesheet>
+                                // needed in case element is self-closing
+                                manifestReader.ReadEndElement(); // </stylesheet>
                             }
                         }
+
                         if (manifestReader.IsStartElement("Settings"))
                         {
                             manifestReader.ReadStartElement("Settings");
@@ -131,33 +131,90 @@ namespace Engage.Dnn.ContentRotator
                                 {
                                     manifestReader.ReadStartElement("Name");
                                     string name = manifestReader.ReadContentAsString();
-                                    manifestReader.ReadEndElement(); //</name>
+                                    manifestReader.ReadEndElement(); // </name>
 
                                     if (manifestReader.IsStartElement("Value"))
                                     {
                                         manifestReader.ReadStartElement("Value");
                                         string value = manifestReader.ReadContentAsString();
-                                        //if element is empty (and self-closing)
+
+                                        // if element is empty (and self-closing)
                                         if (manifestReader.LocalName == "Value")
                                         {
-                                            manifestReader.ReadEndElement(); //</value>
+                                            manifestReader.ReadEndElement(); // </value>
                                         }
 
-                                        _settings.Add(name, value);
+                                        this.settings.Add(name, value);
                                     }
                                 }
-                                manifestReader.ReadEndElement(); //</setting>
+
+                                manifestReader.ReadEndElement(); // </setting>
                             }
-                            manifestReader.ReadEndElement(); //</settings>
+
+                            manifestReader.ReadEndElement(); // </settings>
                         }
-                        manifestReader.ReadEndElement(); //</rotatorManifest>
+
+                        manifestReader.ReadEndElement(); // </rotatorManifest>
                     }
                 }
             }
             catch (FileNotFoundException)
             {
-                //if there is no manifest, just return an empty object.
+                // if there is no manifest, just return an empty object.
                 return;
+            }
+        }
+
+        /// <summary>
+        /// Gets the description.
+        /// </summary>
+        /// <value>The description.</value>
+        public string Description
+        {
+            [DebuggerStepThrough]
+            get
+            {
+                return this.description;
+            }
+        }
+
+        /// <summary>
+        /// Gets the preview image filename.
+        /// </summary>
+        /// <value>The preview image filename.</value>
+        public string PreviewImageFilename
+        {
+            [DebuggerStepThrough]
+            get
+            {
+                return this.previewImageFilename;
+            }
+        }
+
+        /// <summary>
+        /// Gets the settings.
+        /// </summary>
+        /// <value>The settings.</value>
+        public IDictionary<string, string> Settings
+        {
+            [DebuggerStepThrough]
+            get
+            {
+                return new Dictionary<string, string>(this.settings);
+            }
+        }
+
+        /// <summary>
+        /// Gets the stylesheet file path.
+        /// </summary>
+        /// <value>The stylesheet file path.</value>
+        public string StylesheetFilePath
+        {
+            get
+            {
+                return Engage.Utility.HasValue(this.stylesheetFilename)
+                               ? this.templateFilePath + this.stylesheetFilename
+                               : string.Empty;
             }
         }
 
@@ -165,11 +222,16 @@ namespace Engage.Dnn.ContentRotator
         /// Creates a representation of the supplied template
         /// </summary>
         /// <param name="templateName">The name of the template</param>
+        /// <returns>A template manifest</returns>
         /// <exception cref="IOException">If the file cannot be accessed</exception>
         /// <exception cref="XmlSchemaValidationException">If the file is not in a valid format</exception>
         public static TemplateManifest CreateTemplateManifest(string templateName)
         {
-            return new TemplateManifest(Utility.DesktopModuleVirtualPath + Utility.StyleTemplatesFolderName + templateName + "/Manifest.xml", templateName);
+            return
+                    new TemplateManifest(
+                            Utility.DesktopModuleVirtualPath + Utility.StyleTemplatesFolderName + templateName
+                            + "/Manifest.xml",
+                            templateName);
         }
     }
 }
