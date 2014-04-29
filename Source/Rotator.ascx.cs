9f9c06ac3eeb88d5517097ecfe63812ae1cdd0f4
Brian Dukes <bdukes@engagesoftware.com>
2014-04-29 14:15:58 -0500

    Apply random and auto-stop options on server as well as client
    Random didn't effect the first item when only on client.
    Also, no need to send down extra slides if you aren't using them.
    
    Closes #2

--- Source/Rotator.ascx.cs
+++ Source/Rotator.ascx.cs
@@ -16,11 +16,9 @@ namespace Engage.Dnn.ContentRotator
     using System.Diagnostics.CodeAnalysis;
     using System.Globalization;
     using System.Linq;
-    using System.Text;
     using System.Web;
     using System.Web.UI;
     using System.Web.UI.WebControls;
-    using System.Xml;
 
     using DotNetNuke.Entities.Modules;
     using DotNetNuke.Entities.Modules.Actions;
@@ -144,6 +142,19 @@ namespace Engage.Dnn.ContentRotator
                 if (this.slides == null)
                 {
                     this.slides = Slide.GetSlides(this.ModuleId);
+                    if (this.CycleOptions.RandomOrder)
+                    {
+                        var random = new Random();
+                        this.slides = (from slide in this.slides
+                                       let sort = random.Next()
+                                       orderby sort
+                                       select slide).ToList();
+                    }
+
+                    if (this.CycleOptions.AutoStop && this.CycleOptions.AutoStopCount < this.slides.Count)
+                    {
+                        this.slides = this.slides.GetRange(0, this.CycleOptions.AutoStopCount);
+                    }
                 }
                 
                 return this.slides;
