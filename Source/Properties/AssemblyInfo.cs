74e339ad45c7557d8da6b476c3733e8d5679d134
Brian Dukes <bdukes@engagesoftware.com>
2014-04-29 14:18:44 -0500

    Copyright bump

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿// <copyright file="AssemblyInfo.cs" company="Engage Software">
 // Engage: Rotator - http://www.engagemodules.com
-// Copyright (c) 2004-2012
+// Copyright (c) 2004-2014
 // by Engage Software ( http://www.engagesoftware.com )
 // </copyright>
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
@@ -19,7 +19,7 @@ using System.Web.UI;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Engage Software")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Copyright © Engage Software 2012")]
+[assembly: AssemblyCopyright("Copyright © Engage Software 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
